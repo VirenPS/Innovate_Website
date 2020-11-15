c35ca9b627b846097d3a7553852d2fffa29cae6c
VirenPS <viren.samani@hotmail.co.uk>
2020-11-15 22:50:37 +0000

    .

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from "./Components/Navbar";
 import Section from "./Components/Section";
 import dummyText from "./DummyText";
 
+// import {contentsSection1, contentsSection2, contentsSection3, contentsSection4, contentsSection5} from "./TextContents";
+
 class App extends Component {
   render() {
     return (
@@ -11,43 +13,39 @@ class App extends Component {
         <Navbar />
         
           {/* title="Section 1" */}
-          {/* subtitle={dummyText} */}
+          {/* subtitle={textContents} */}
           {/* dark={true} */}
           {/* id="section1" */}
 
         <div className={"section" + (false ? " section-dark" : "")}>
           <div className="section-content" id="section1" >
-            <h1>"Section 1"</h1>
-            <p>{dummyText}
-
-            </p>
-
+            <h1>Hello Piyush</h1>
+            <p>"HIII"</p>
           </div>
         </div>
-
 
         <Section
           title="Section 2"
           subtitle={dummyText}
-          dark={true}
+          dark={false}
           id="section2"
         />
         <Section
           title="Section 3"
           subtitle={dummyText}
-          dark={false}
+          dark={true}
           id="section3"
         />
         <Section
           title="Section 4"
           subtitle={dummyText}
-          dark={true}
+          dark={false}
           id="section4"
         />
         <Section
           title="Section 5"
           subtitle={dummyText}
-          dark={false}
+          dark={true}
           id="section5"
         />
       </div>
